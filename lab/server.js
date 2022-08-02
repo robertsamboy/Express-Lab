81f6dba2ca7bad37b9783c7ebe04334acd7a55b8
robertsamboy <106219442+robertsamboy@users.noreply.github.com>
2022-08-02 09:56:33 -0400

    adding another template

--- lab/server.js
+++ lab/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express(); 
 const port = 3000;
 
+const boroughs = ['THE BRONX', 'brooklyn','queens','manhatten','staten island'] 
+
+
 const fs = require('fs') // this engine requires the fs module like we did Saturday
 app.engine('hypatia', (filePath, options, callback) => { // define the view engine called hypatia
   fs.readFile(filePath, (err, content) => {
@@ -14,29 +17,32 @@ app.engine('hypatia', (filePath, options, callback) => { // define the view engi
     return callback(null, rendered)
   })
 })
+app.engine('adds', (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content)=>{
+        if (err) return callback(err)
+        const render = content.toString()
+        .replace()
+
+    })
+})
+
+
+
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'hypatia') // register the hypatia view engine
-
-app.get('/dog', (req,res)=> {res.render('template', {title:'A dog', message: 'This here is a dog',img:'https://cdn.pixabay.com/photo/2016/01/05/17/51/maltese-1123016__480.jpg', content: 'This dog is not available for sell'})
+app.set('view engine','adds')
+app.get('/dog', (req,res)=> {res.render('template.hypatia', {title:'A dog', message: 'This here is a dog',img:'https://cdn.pixabay.com/photo/2016/01/05/17/51/maltese-1123016__480.jpg', content: 'This dog is not available for sell'})
 })
 
 
 
 app.get('/test', (req,res) => {
-    res.send('hey')
+    res.send('TESTING')
 })
 
-
-
-
-
-
-
-
-
-
-
-
+app.get('/:boroughsofnyc', (req, res) => {
+    res.send(boroughs[req.params.boroughsofnyc])
+});
 
 
 app.listen(port,() => {
